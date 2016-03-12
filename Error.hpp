205eb8bf4c717482ad9272fa72a5772c85a76c3b
port-jiro-iwai <jiro.iwai@theport.jp>
2016-03-12 22:52:51 +0900

    RES-25 modified,added Errorクラスを分離
    リファクタリング

---
+++ Error.hpp
@@ -0,0 +1,14 @@
+#ifndef ERROR__H
+#define ERROR__H
+
+#include <string>
+
+class Error {
+public:
+	Error(const std::string& arg) : msg(arg) {}
+	const std::string& what() { return msg; }
+private:
+	std::string msg;
+};
+
+#endif // ERROR__H
--- Main.cpp
+++ Main.cpp
@@ -5,14 +5,7 @@
 #include "SemiCrf.hpp"
 #include "AppReqs.hpp"
 #include "DebugOut.hpp"
-
-class Error {
-public:
-	Error(const std::string& arg) : msg(arg) {}
-	const std::string& what() { return msg; }
-private:
-	std::string msg;
-};
+#include "Error.hpp"
 
 class Options {
 public:
