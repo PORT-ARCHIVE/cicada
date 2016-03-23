df47a76f445a8d26a94624639f3f97c7bf37034e
Jiro Iwai <jiro.iwai@theport.jp>
2016-03-23 18:28:50 +0900

    RES-72 modified テストデータ生成プログラムセグメント表示対応
    デバッグ容易性向上

--- Generator.cpp
+++ Generator.cpp
@@ -1,8 +1,11 @@
 // © 2016 PORT INC.
 
 #include <iostream>
+#include <vector>
+#include <sstream>
 #include <boost/lexical_cast.hpp>
 #include <boost/random.hpp>
+#include "Logger.hpp"
 #include "Error.hpp"
 
 using namespace boost;
@@ -83,26 +86,16 @@ int f_y2y(int y, double r)
 class Options {
 public:
 	Options()
-		: training_data_file("")
-		, inference_data_file("")
-		, format("digit")
-		, weights_file("weights.txt")
-		, maxLength(5)
-		, maxIteration(1024)
-		, e0(1.0e-5)
-		, e1(1.0e-5)
-		, debug_level(0) {};
+		: length(16)
+		, iteration(16)
+		, seed(42)
+		, logLevel(0) {};
 	void parse(int argc, char *argv[]);
 public:
-	std::string training_data_file;
-	std::string inference_data_file;
-	std::string format;
-	std::string weights_file;
-	int maxLength;
-	int maxIteration;
-	double e0;
-	double e1;
-	int debug_level;
+	int length;
+	int iteration;
+	int seed;
+	int logLevel;
 };
 
 void Options::parse(int argc, char *argv[])
@@ -111,24 +104,14 @@ void Options::parse(int argc, char *argv[])
 
 		for( int i = 1; i < argc; i++ ) {
 			std::string arg = argv[i];
-			if( arg == "-t" ) {
-				training_data_file = argv[++i];
-			} else if( arg == "-i" ) {
-				inference_data_file = argv[++i];
-			} else if( arg == "-f" || arg == "--input-file-format" ) {
-				format = argv[++i];
-			} else if( arg == "-l" || arg == "--max-length") {
-				maxLength = boost::lexical_cast<int>(argv[++i]);
-			} else if( arg == "-r" || arg == "--max-iteration") {
-				maxIteration = boost::lexical_cast<int>(argv[++i]);
-			} else if( arg == "-e0" ) {
-				e0 = boost::lexical_cast<double>(argv[++i]);
-			} else if( arg == "-e1" ) {
-				e1 = boost::lexical_cast<double>(argv[++i]);
-			} else if( arg == "-w" ) {
-				weights_file = argv[++i];
-			} else if( arg == "--debug-level" ) {
-				debug_level = boost::lexical_cast<int>(argv[++i]);
+			if( arg == "-l" || arg == "--length") {
+				length = lexical_cast<int>(argv[++i]);
+			} else if( arg == "-r" || arg == "--iteration") {
+				iteration = lexical_cast<int>(argv[++i]);
+			} else if( arg == "-s" ) {
+				seed = lexical_cast<int>(argv[++i]);
+			} else if( arg == "--log-level" ) {
+				logLevel = lexical_cast<int>(argv[++i]);
 			} else {
 				std::stringstream ss;
 				ss << "error: unknown option specified";
@@ -137,11 +120,8 @@ void Options::parse(int argc, char *argv[])
 		}
 
 	} catch(Error& e) {
-
 		throw e;
-
 	} catch(...) {
-
 		std::stringstream ss;
 		ss << "error: invalid option specified";
 		throw Error(ss.str());
@@ -150,22 +130,93 @@ void Options::parse(int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
-	preProcess();
+	int ret = 0x0;
 	
-	const int N = 100;
-	int yi = 0; // T.B.D
+	Options options;
+	options.parse(argc, argv);
+	Logger::setLevel(options.logLevel);
 
-	minstd_rand gen( 42 );
-	uniform_real<> dst( 0, 1 );
-	variate_generator<minstd_rand&, uniform_real<> > rand( gen, dst );
-
-	std::cout << yi << "\t" << f_y2x(yi, rand()) << std::endl;
-	
-	for( int i = 1; i < N; i++ ) {
-		
-		yi = f_y2y(yi, rand());
-		std::cout << yi << "\t" << f_y2x(yi, rand()) << std::endl;
+	if( options.length < 3 ) {
+		std::stringstream ss;
+		ss << "length must be greater than 2";
+		throw Error(ss.str());
 	}
 
-	return 0;
+	try {
+
+		preProcess();
+	
+		minstd_rand gen( options.seed );
+		uniform_real<> dst( 0, 1 );
+		variate_generator<minstd_rand&, uniform_real<> > rand( gen, dst );
+
+		for( int j = 0; j < options.iteration; j++ ) {
+
+			Logger::out(0) << "# BEGIN" << std::endl;
+
+			std::vector<int> y;
+			std::vector<int> x;
+			for( int i = 0; i < options.length; i++ ) {
+				int yi = f_y2y(yi, rand()); y.push_back(yi);
+				int xi = f_y2x(yi, rand());	x.push_back(xi);
+			}
+
+			auto xi = x.begin();
+			auto yi = y.begin();
+			auto yj = y.begin(); yj += 1;
+			auto yk = y.begin(); yk += 2;
+
+			Logger::out(0) << *xi << "\t";
+
+			if( *yi == *yj ) {
+				Logger::out(0) << "S\t";
+			} else {
+				Logger::out(0) << "S/E\t";
+			}
+
+			Logger::out(0) << *yi << std::endl;
+
+			++xi;
+			for( ; yk != y.end(); ++xi, ++yi, ++yj, ++yk ) {
+				Logger::out(0) << *xi;
+				if( *yi != *yj && *yj == *yk ) {
+					Logger::out(0) << "\tS\t";
+				} else if( *yi == *yj && *yj == *yk ) {
+					Logger::out(0) << "\tM\t";
+				} else if( *yi != *yj && *yj != *yk ) {
+					Logger::out(0) << "\tS/E\t";
+				} else if( *yi == *yj && *yj != *yk ) {
+					Logger::out(0) << "\tE\t";
+				} else {
+					assert(0);
+				}
+				Logger::out(0) << *yj << std::endl;
+			}
+
+			Logger::out(0) << *xi;
+
+			if( *yi != *yj ) {
+				Logger::out(0) << "\tS/E\t";
+			} else if( *yi == *yj ) {
+				Logger::out(0) << "\tE\t";
+			} else {
+				assert(0);
+			}
+
+			Logger::out(0) << *yj << std::endl;
+			Logger::out(0) << "# END" << std::endl;
+		}
+
+	} catch(Error& e) {
+
+		std::cerr << "error: " << e.what() << std::endl;
+		ret = 0x1;
+		
+	} catch(...) {
+
+		std::cerr << "error: unexpected exception" << std::endl;
+		ret = 0x2;
+	}
+
+	exit(ret);
 }
