85a5736b5163ac38cc5d6ab51c5c46816c668fc9
Jiro-Iwai <jiro.iwai@gmail.com>
2016-04-10 03:04:16 +0900

    RES-159 modified 不要なヘッダ、typedefを削除
    リファクタリング

--- W2V.hpp
+++ W2V.hpp
@@ -6,14 +6,12 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/vector.hpp>
 
 namespace W2V {
 
     using namespace boost::numeric::ublas;
     typedef boost::numeric::ublas::vector<double> vector;
-    typedef boost::numeric::ublas::matrix<double> matrix;
 
  	class Matrix {
 	public:
