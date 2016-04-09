b53631c19d43e96d5c57ec7e109ae5a05e8139f4
Jiro-Iwai <jiro.iwai@gmail.com>
2016-04-10 02:58:10 +0900

    RES-159 modified w2vのwrapper実装
    機能拡張　

--- W2V.cpp
+++ W2V.cpp
@@ -1,7 +1,8 @@
 // © 2016 PORT INC.
 
 #include <W2V.hpp>
-#include <fstream>
+#include <cstdlib>
+#include "Error.hpp"
 
 namespace W2V {
 
@@ -13,20 +14,66 @@ namespace W2V {
 	{
 	}
 
-	void read(std::string file)
+	void Matrix::read(std::string file)
  	{
-		std::ifstream ifs;
+        FILE* f = std::fopen(file.c_str(), "rb");
+   	    if( f == NULL ) {
+			throw Error("cannot open such file");
+        }
+
+        long long words;
+     	fscanf(f, "%lld", &words);
+
+        long long size;
+    	fscanf(f, "%lld", &size);
+
+        M.resize(words);
+
+        char c;
+		for( long long i = 0; i < words; i++ ) {
+
+            std::string w;
+
+            while(1) {
+                c = std::fgetc(f);
+                if( c == '\n' ) {
+                    continue;
+                } else if( std::feof(f) || c == ' ' ) {
+                    w2i_[w] = i;
+                    i2w_[i] = w;
+		 			break;
+		 		}
+		 		w.push_back(c);
+		 	}
+
+            vector& v = M[i];
+			v.resize(size);
+		 	for( long long j = 0; j < size; j++ ) {
+		 		float e;
+                std::fread((char*)&e, sizeof(float), 1, f);
+		 		v(j) = e;
+		 	}
+
+		 	double n = norm_2(v);
+		 	v = v/n;
+		}
+
+        std::fclose(f);
 	}
 
-	int Matrix::w2i(std::string w)
+	int Matrix::w2i(const std::string& w)
 	{
-		return 0;
+        auto ip = w2i_.find(w);
+        if( ip != w2i_.end() ) {
+            return ip->second;
+        } else {
+            throw Error("");
+        }
 	}
 
-	vector Matrix::i2v(int i)
+	const vector& Matrix::i2v(int i)
 	{
-		vector v;
-		return ( std::move(v) );
+		const vector& v = M[i];
+		return v;
 	}
-
 }
--- W2V.hpp
+++ W2V.hpp
@@ -4,24 +4,28 @@
 #define W2V__H
 
 #include <string>
+#include <vector>
+#include <map>
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/vector.hpp>
 
-namespace W2V { // word2vecのWrapper
+namespace W2V {
 
-	using namespace boost::numeric::ublas;
+    using namespace boost::numeric::ublas;
     typedef boost::numeric::ublas::vector<double> vector;
     typedef boost::numeric::ublas::matrix<double> matrix;
 
  	class Matrix {
 	public:
 		Matrix();
-		virtual ~Matrix();
+        virtual ~Matrix();
 		void read(std::string file);
-		int w2i(std::string w);
-		vector i2v(int i);
-	private:
-		matrix m;
+		int w2i(const std::string& w);
+		const vector& i2v(int i);
+    private:
+        std::map<std::string, long long> w2i_;
+        std::map<long long, std::string> i2w_;
+        std::vector<vector> M;
 	};
 }
 
