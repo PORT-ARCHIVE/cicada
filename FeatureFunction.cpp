ac67a3a0c105fb28f2dd7e5ef0c0902e31c5dbc6
Jiro Iwai <jiro.iwai@theport.jp>
2016-09-14 15:37:41 +0900

    RES-1202 modify 素性に駅名を暫定で考慮

--- FeatureFunction.cpp
+++ FeatureFunction.cpp
@@ -269,8 +269,8 @@ namespace App {
 
 	double Jpn::place_feature(const std::vector<std::string>& word)
 	{
-		static std::set<std::string> prefecture_divisions { "都","道","府","県" };
-		static std::set<std::string> sub_divisions { "市", "区", "町", "村", "郡", "字" };
+		static std::set<std::string> prefecture_divisions { "都","道","府","県","州","省" };
+		static std::set<std::string> sub_divisions { "市", "区", "町", "村", "郡", "字", "大字", "小字" };
 		static std::set<std::string> prefecture_names { "北海","青森","岩手","宮城","秋田","山形","福島","茨城","栃木",
 				"群馬","埼玉","千葉","東京都","神奈川","新潟","富山","石川","福井","山梨","長野","岐阜","静岡","愛知","三重",
 				"滋賀","京都","大阪","兵庫","奈良","和歌山","鳥取","島根","岡山","広島","山口","徳島","香川","愛媛","高知",
@@ -283,15 +283,18 @@ namespace App {
 		int is_head_prefecture = 0;
 		int is_none_area_relate	= 0;
 		int is_prefecture_name = 0;
+		int is_station = 0;
 		std::map<std::string, int> is_sub_division;
 		std::string prefecture_name;
 
+		int i = 0;
+		int s = word.size();
 		bool is_first = true;
 		for( auto& w : word ) {
 
 			bool is_none_area_relate_check = true;
 
-			// 都道府県のどれか
+			// 都道府県州省のどれか
 			if( prefecture_divisions.find(w) != prefecture_divisions.end() ) {
 				is_prefecture++;
 				if( is_first ) {
@@ -313,7 +316,7 @@ namespace App {
 				is_none_area_relate_check = false;
 			}
 
-			// 市町村郡字のどれか
+			// 市町村郡(大/小)字のどれか
 			if( sub_divisions.find(w) != sub_divisions.end() ) {
 				is_sub_division[w]++;
 				if( is_first ) {
@@ -322,16 +325,22 @@ namespace App {
 				is_none_area_relate_check = false;
 			}
 
+			if( w == "駅" && 1 < s && i == s-1 ) {
+				is_station++;
+			}
+
 			// 地名関連語ではない
 			if( is_none_area_relate_check ) {
 				is_none_area_relate++;
 			}
 
 			is_first = false;
+			i++;
 		}
 
 		feature += is_area;
 		feature += is_prefecture;
+		feature += is_station;
 		for( auto& p : is_sub_division ) {
 			feature += p.second;
 		}
